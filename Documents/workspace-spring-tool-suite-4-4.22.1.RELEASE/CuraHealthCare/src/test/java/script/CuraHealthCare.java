db7beb0f9a34f03f936cf45671711bea22349685
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-06-27 00:28:26 +0530

    script added for components dropdown, checkbox and radio button

--- Documents/workspace-spring-tool-suite-4-4.22.1.RELEASE/CuraHealthCare/src/test/java/script/CuraHealthCare.java
+++ Documents/workspace-spring-tool-suite-4-4.22.1.RELEASE/CuraHealthCare/src/test/java/script/CuraHealthCare.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,14 +22,25 @@ public class CuraHealthCare {
 	}
 	
 	@Test
-	public void automationscript() {
+	public void automationscript() throws InterruptedException {
 		WebElement appointment_button = driver.findElement(By.id("btn-make-appointment"));
 		appointment_button.click();
 		
 		driver.findElement(By.id("txt-username")).sendKeys("John Doe");
 		driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+		driver.findElement(By.id("btn-login")).click();
 		
 		
+		WebElement facility_dropdown = driver.findElement(By.id("combo_facility"));
+		Select sel = new Select(facility_dropdown);
+		sel.selectByVisibleText("Seoul CURA Healthcare Center");
+		
+		
+		driver.findElement(By.className("checkbox-inline")).click();
+		driver.findElement(By.id("radio_program_none")).click();
+		driver.findElement(By.id("txt_visit_date")).click();
+		
+		Thread.sleep(3000);
 	}
 	
 	@AfterMethod
